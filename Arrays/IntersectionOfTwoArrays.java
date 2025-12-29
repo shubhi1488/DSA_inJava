4fe9bfee1b31e33f1d6b25c165d4561d7385dc02
Shubhi Agarwal <shubhi1488@gmail.com>
2025-12-29 22:29:19 +0530

    Create IntersectionOfTwoArrays.java

---
+++ Arrays/IntersectionOfTwoArrays.java
@@ -0,0 +1,52 @@
+//brute force-
+package Arrays;
+import java.util.*;
+public class Intersectionoftwoarrays {
+    public int[] intersection(int[] nums1,int[] nums2){
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int[] visited=new int[n2];
+        List<Integer> arrList=new ArrayList<>();
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(nums1[i]==nums2[j] && visited[j]==0){
+                    arrList.add(nums2[j]);
+                    visited[j]=1;
+                    break;
+                }
+                else if(nums2[j]>nums1[i]) break;
+
+            }
+        }
+        int[] ans=new int[arrList.size()];
+        for(int k=0;k<arrList.size();k++){
+            ans[k]=arrList.get(k);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Intersectionoftwoarrays i1=new Intersectionoftwoarrays();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of the first array");
+        int n1=sc.nextInt();
+        System.out.println("enter size of second array");
+        int n2=sc.nextInt();
+        System.out.println("enter elements in first array");
+        int[] arr1=new int[n1];
+        for(int i=0;i<n1;i++){
+            arr1[i]=sc.nextInt();
+        }
+        System.out.println("enter elements in second array");
+        int[] arr2=new int[n2];
+        for(int i=0;i<n2;i++){
+            arr2[i]=sc.nextInt();
+        }
+        int[] intersection=i1.intersection(arr1,arr2);
+        System.out.println("intersected list-");
+        for(int i=0;i<intersection.length;i++){
+            System.out.print(intersection[i]+" ");
+        }
+        System.out.println();
+    }
+}

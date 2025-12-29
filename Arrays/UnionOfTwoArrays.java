7a60ad46bcd7c6d21344ac01ed5b993bb0e3bf47
Shubhi Agarwal <shubhi1488@gmail.com>
2025-12-29 21:56:27 +0530

    Update UnionOfTwoArrays.java

--- Arrays/UnionOfTwoArrays.java
+++ Arrays/UnionOfTwoArrays.java
@@ -1,3 +1,4 @@
+//Brute Force-
 package Arrays;
 import java.util.*;
 public class UnionOfTwoArrays {
@@ -40,5 +41,46 @@ public class UnionOfTwoArrays {
             System.out.print(x+" ");
         }
         System.out.println();
+    }
+}
+
+//Optimal- Two pointer approach
+class Solution {
+    public int[] unionArray(int[] nums1, int[] nums2) {
+       List<Integer>UnionList=new ArrayList<>();
+       int i=0,j=0;
+       int n=nums1.length;
+       int m=nums2.length;
+       while(i<n && j<m){
+        if(nums1[i]<=nums2[j]){
+            if(UnionList.isEmpty() || UnionList.get(UnionList.size()-1)!=nums1[i]){
+                UnionList.add(nums1[i]);
+            }
+            i++;
+        }
+        else{
+            if(UnionList.isEmpty() || UnionList.get(UnionList.size()-1)!=nums2[j]){
+                UnionList.add(nums2[j]);
+            }
+            j++;
+        }
+       }
+       while(i<n){
+        if(UnionList.isEmpty() || UnionList.get(UnionList.size()-1)!=nums1[i]){
+            UnionList.add(nums1[i]);
+        }
+        i++;
+       }
+       while(j<m){
+        if(UnionList.isEmpty() || UnionList.get(UnionList.size()-1)!=nums2[j]){
+            UnionList.add(nums2[j]);
+        }
+        j++;
+       }
+       int[] union=new int[UnionList.size()];
+       for(int k=0;k<UnionList.size();k++){
+        union[k]=UnionList.get(k);
+       }
+       return union;
     }
 }

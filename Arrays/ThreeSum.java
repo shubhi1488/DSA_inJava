4fdbf23b8f40ddb372be12a49ebd9e6a00e6c7e7
Shubhi Agarwal <shubhi1488@gmail.com>
2026-01-15 14:27:46 +0530

    Update ThreeSum.java

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -1,3 +1,4 @@
+//Brute Force
 package Arrays;
 import java.util.*;
 public class ThreeSum {
@@ -43,4 +44,29 @@ public class ThreeSum {
         System.out.println();
     }
 
+}
+//Better Approach
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+               Set<List<Integer>>tripletSet=new HashSet<>();
+       int n=nums.length;
+       for(int i=0;i<n;i++){
+        Set<Integer>hashset=new HashSet<>();
+        for(int j=i+1;j<n;j++){
+            int third=-(nums[i]+nums[j]);
+            if(hashset.contains(third)){
+                List<Integer>temp=new ArrayList<>();
+                temp.add(nums[i]);
+                temp.add(nums[j]);
+                temp.add(third);
+                Collections.sort(temp);
+                tripletSet.add(temp);
+            }
+            hashset.add(nums[j]);
+        }
+       }
+       List<List<Integer>>ans=new ArrayList<>(tripletSet);
+       return ans;
+
+    }
 }
